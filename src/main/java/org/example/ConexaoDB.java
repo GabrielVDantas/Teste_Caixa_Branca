039b6141055d8a5500654d1a9a319278e94e042c
GabrielVDantas <gvdantas2028@gmail.com>
2024-04-23 19:30:30 -0300

    gerando JavaDoc

--- src/main/java/org/example/ConexaoDB.java
+++ src/main/java/org/example/ConexaoDB.java
@@ -9,6 +9,10 @@ public class ConexaoDB {
 
     private Connection con = null;
 
+    /**
+     *
+     * @return Retorna a conexao caso seja estabelecida ou retorna null caso nao consiga
+     */
     public Connection conexao() {
         try {
             Class.forName("org.gjt.mm.mysql.Driver").newInstance();
@@ -20,11 +24,18 @@ public class ConexaoDB {
         return con;
     }
 
+    /**
+     *
+     * @param login Recebe a informacao de login passada pelo usuario
+     * @param senha Recebe a informacao de senha passada pelo usuario
+     * @return Verifica login e senha no DB, retorna verdadeiro se existir, falso se nao existir
+     */
     public boolean verifica(String login, String senha) {
         conexao();
         try {
             Statement st = con.createStatement();
-            ResultSet rs = st.executeQuery("select * from usuarios where login='" + login + "' and senha='" + senha + "'");
+            ResultSet rs = st.executeQuery
+                    ("select * from usuarios where login= '" + login + "' and senha= '" + senha + "'");
             if(rs.next()) {
                 return true;
             }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,16 @@
 package org.example;
 
+/**
+ * @author Gabriel Viegas Dantas
+ * @version 1.0
+ * @since Release 1.0 da aplicacao
+ */
+
 public class Main {
     public static void main(String[] args) {
 
         var conexaoDB = new ConexaoDB();
 
-        var resultado = conexaoDB.verifica("loginTeste", "senhaTeste");
-        System.out.println(resultado);
+        conexaoDB.verifica("loginTeste", "senhaTeste");
     }
 }
